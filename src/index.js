629b71c522f2f0ab25deaaed54b11927cf9518ae
marmooo <marmooo@users.noreply.github.com>
2025-03-02 00:01:54 +0900

    improve UI

--- src/index.js
+++ src/index.js
@@ -526,7 +526,7 @@ function resizeFontSize(node) {
   const font = style.fontFamily;
   const fontSize = parseFloat(style.fontSize);
   const lineHeight = parseFloat(style.lineHeight) / fontSize;
-  const nodeHeight = document.getElementById("aaOuter").offsetHeight;
+  const nodeHeight = globalThis.innerHeight - 340;
   const nodeWidth = infoPanel.clientWidth;
   const nodeRect = [nodeWidth, nodeHeight];
   const textRect = getTextRect(node.textContent, fontSize, font, lineHeight);
